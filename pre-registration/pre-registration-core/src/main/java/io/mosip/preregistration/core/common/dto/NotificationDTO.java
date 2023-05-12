469d1e006b5ec09eecc3987e61a79f9fb53c1250
avanishduggireddy <avanish.duggireddy@infystrat.com>
2023-05-12 13:10:37 +0530

    Revert "pre booking notification feature added"
    This reverts commit df8ff73660a87ca463f08f874dd062d69fb23f53.

--- pre-registration/pre-registration-core/src/main/java/io/mosip/preregistration/core/common/dto/NotificationDTO.java
+++ pre-registration/pre-registration-core/src/main/java/io/mosip/preregistration/core/common/dto/NotificationDTO.java
@@ -49,10 +49,7 @@ public class NotificationDTO implements Serializable {
 	 * additionalRecipient for notififcation
 	 */
 	private boolean additionalRecipient;
-	/**
-	 * flag to check pre booking notification
-	 */
-	private String isPreBookingNotification;
+	
 	/**
 	 * batch config field
 	 */
